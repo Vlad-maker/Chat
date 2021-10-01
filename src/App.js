1828efafaba083f69d33b9f88bcb770a3aefc08b
Vlad-maker <skateislife@mail.ru>
2021-10-01 12:09:52 +0300

    Feat: Sender + logic added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from "./components/Header/Header";
 import Dialog from "./components/Dialog/Dialog";
+import Sender from "./components/Sender/Sender";
 
 const App = () => {
+  const [ message, addMessage ] = useState(null);
+
   return (
     <div className="container">
         <Header />
-        <Dialog />
+        <Dialog newMessage={message}/>
+        <Sender onAddMessage={addMessage}/>
     </div>
   )
 }
--- src/components/Dialog/Dialog.jsx
+++ src/components/Dialog/Dialog.jsx
@@ -1,21 +1,65 @@
-import React from 'react';
+import React, { useReducer, useEffect, useRef } from 'react';
+import PropTypes from "prop-types";
 import Item from './Item/Item';
 import { normalizeDialog } from './Item/helpers';
+import reducer from './reducer';
 import data from '../../data';
 import './dialog.css';
 
-const Dialog = () => {
-    const normalizedDialog = normalizeDialog(data);
+const Dialog = ({ newMessage }) => {
+    const dialogRef = useRef();
+    const [state, dispatch] = useReducer(reducer, {
+        messages: data,
+    });
+
+    useEffect(() => {
+        if (newMessage) {
+            dispatch({
+                type: "add-message",
+                payload: newMessage,
+            });
+
+            setTimeout(() => {
+                dispatch({
+                    type: "update-status",
+                    payload: {
+                        id: newMessage.id,
+                        status: "readed",
+                    },
+                });
+            }, 1000);
+        }
+    }, [newMessage]);
+
+    useEffect(() => {
+        dialogRef.current.scrollTop = dialogRef.current.scrollHeight;
+    }, [state.messages.length]);
+
+    const onRemove = (id) => {
+        dispatch({
+            type: "remove-message",
+            payload: id,
+        });
+    };
+
+    const normalizedDialog = normalizeDialog(state.messages);
 
     return (
         <div className="dialog">
-            <div className="overflow">
+            <div className="overflow" ref={dialogRef}>
                 {normalizedDialog.map((item) => (
-                    <Item {...item} key={item.id} />
+                    <Item {...item} key={item.id}  onRemove={onRemove} />
                 ))}
             </div>
         </div>
     );
 };
 
+Dialog.propTypes = {
+    newMessage: PropTypes.shape({
+        id: PropTypes.number,
+    }),
+};
+
 export default Dialog;
+
