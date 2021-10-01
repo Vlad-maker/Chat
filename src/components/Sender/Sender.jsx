f28cdee629ec26cc17e9c309b735cdb5b9976002
Vlad-maker <skateislife@mail.ru>
2021-10-01 13:16:22 +0300

    Fix: big with avatar fixed

--- src/components/Sender/Sender.jsx
+++ src/components/Sender/Sender.jsx
@@ -1,46 +1,3 @@
-// import React, { useState } from "react";
-// import PropTypes from "prop-types";
-// import "./sender.css";
-
-// const Sender = ({ onAddMessage }) => {
-//   const [value, setValue] = useState("");
-
-//   const onChange = (event) => setValue(event.target.value);
-//   const onSubmit = (event) => {
-//     event.preventDefault();
-
-//     onAddMessage({
-//       id: Date.now(),
-//       avatar:
-//         "https://sun9-58.userapi.com/c836638/v836638514/867c/SPMigNB8gw0.jpg",
-//       message: value,
-//       date: new Date().toISOString(),
-//       is: "my",
-//       status: "sended",
-//     });
-//     setValue("");
-//   };
-
-//   return (
-//     <form className="sender" onSubmit={onSubmit}>
-//       <input
-//         placeholder="Введите сообщение"
-//         value={value}
-//         onChange={onChange}
-//         required
-//       />
-//       <button>Отправить</button>
-//     </form>
-//   );
-// };
-
-// Sender.propTypes = {
-//   onAddMessage: PropTypes.func.isRequired,
-// };
-
-// export default Sender;
-
-
 import React, { useState } from 'react';
 import PropTypes from "prop-types";
 import "./sender.css"
@@ -54,7 +11,7 @@ const Sender = ({onAddMessage}) => {
 
         onAddMessage({
             id: Date.now(),
-            avatar: "./src/img/резюме.webp",
+            avatar: "https://sun9-58.userapi.com/c836638/v836638514/867c/SPMigNB8gw0.jpg",
             message: value,
             date: new Date().toISOString(),
             is: "my",
